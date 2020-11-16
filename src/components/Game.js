418cc3301992ef6767db6a98252218ca02bf594a
‘Juliano <julianoperins@gmail.com>
2020-11-16 16:47:20 -0500

    fetched data and consoled log

---
+++ src/components/Game.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// Style / Animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+const Game = () => {
+  return <div></div>;
+};
+
+export default Game;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,30 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
+
+// components
+
+// Style / Animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+import Game from "../components/Game";
 
 const Home = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(loadGames());
-  });
+  }, [dispatch]);
+  //   Getting the data back
+  const { popular, newGames, upcoming } = useSelector((state) => state.games);
 
   return (
-    <div>
+    <GameList>
       <h1>Home</h1>
-    </div>
+    </GameList>
   );
 };
+
+const GameList = styled(motion.div)``;
 
 export default Home;
