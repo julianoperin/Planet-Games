36b53e4736d4880979714926ba96416f4bc35a2d
‘Juliano <julianoperins@gmail.com>
2020-11-17 21:25:34 -0500

    Added the nav bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React, { useEffect } from "react";
+import React from "react";
+//Components and pages
 import Home from "./pages/Home";
-
-// Global Styles
+import Nav from "./components/Nav";
+//Styles
 import GlobalStyles from "./components/GlobalStyles";
+//Router
 import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyles />
+      <Nav />
       <Route path={["/game/:id", "/"]}>
         <Home />
       </Route>
---
+++ src/components/Nav.js
@@ -0,0 +1,58 @@
+import React from "react";
+// Animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import logo from "../img/logo.svg";
+
+const Nav = () => {
+  return (
+    <StyledNav>
+      <Logo>
+        <img src={logo} alt="logo" />
+        <h1> Planet Games</h1>
+      </Logo>
+      <div className="search">
+        <input type="text" />
+        <button>Search</button>
+      </div>
+    </StyledNav>
+  );
+};
+const StyledNav = styled(motion.nav)`
+  padding: 3rem 5rem;
+  text-align: center;
+  input {
+    width: 30%;
+    font-size: 1.5rem;
+    padding: 0.5rem;
+    border: none;
+    margin-top: 1rem;
+    box-shadow: 0px 0px 30px rgba(0, 0, 0, 0.2);
+    border-top-left-radius: 25px;
+    border-bottom-left-radius: 25px;
+  }
+  button {
+    font-size: 1.5rem;
+    border: none;
+    padding: 0.5rem 2rem;
+    cursor: pointer;
+    background: #ff7676;
+    color: white;
+    border-top-right-radius: 25px;
+    border-bottom-right-radius: 25px;
+  }
+`;
+
+const Logo = styled(motion.div)`
+  display: flex;
+  justify-content: center;
+  padding: 1rem;
+  cursor: pointer;
+  img {
+    height: 3rem;
+    width: 3rem;
+    margin-right: 1rem;
+  }
+`;
+
+export default Nav;
